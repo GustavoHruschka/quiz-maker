6815c05c44b9332f4910917864aaa3fa2d1e7570
GustavoHruschka <gustavo.phruschka@gmail.com>
2020-08-14 22:32:47 -0300

    layout changed again

--- website/src/components/CreateQuizItem/index.tsx
+++ website/src/components/CreateQuizItem/index.tsx
@@ -5,6 +5,7 @@ import './styles.css'
 interface myState {
     questionText: String
     optionsText: Array<String>
+    correctOptionIndex: number
 }
 
 class CreateQuizItem extends React.Component<{}, myState> {
@@ -12,7 +13,8 @@ class CreateQuizItem extends React.Component<{}, myState> {
         super(props)
         this.state = {
             questionText: '',
-            optionsText: ['', '']
+            optionsText: ['', ''],
+            correctOptionIndex: 0,
         }
     }
 
@@ -20,14 +22,10 @@ class CreateQuizItem extends React.Component<{}, myState> {
         this.setState(previousState => ({
             optionsText: [...previousState.optionsText, '']
         }))
-
-        console.log('this.state')
     }
 
     handleQuestionTextChange(event: React.FormEvent<HTMLTextAreaElement>) {
         this.setState({ questionText: event.currentTarget.value })
-
-        console.log(this.state)
     }
 
     render() {
@@ -42,12 +40,33 @@ class CreateQuizItem extends React.Component<{}, myState> {
                 <div className="answer">
                     {this.state.optionsText.map((value, index) => {
                         return (
-                            <input
-                                key={index}
-                                type="text"
-                                className="options"
-                                placeholder={`Option `+ (index + 1)}
-                            />
+                            <div
+                                className="option-box"
+                                key={'box' + index}
+                            >
+
+                                <input
+                                    className="options"
+                                    key={'text' + index}
+                                    type="text"
+                                    placeholder={`Option ` + (index + 1)}
+                                />
+                                <div
+                                    className="select-right-option-box"
+                                    style={{
+                                        backgroundColor:
+                                            this.state.correctOptionIndex === index ? '#59eb4c' : '#eb5f4c'
+                                    }}
+                                >
+                                    <input
+                                        className="select-right-option"
+                                        key={'radio' + index}
+                                        type="radio"
+                                        name="select-right-option"
+                                        value={index}
+                                    />
+                                </div>
+                            </div>
                         )
                     })}
 
